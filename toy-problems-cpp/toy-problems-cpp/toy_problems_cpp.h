997dbc31019328e05603adacece811cfdc982fec
David Raleigh <davidraleigh@gmail.com>
2015-01-22 11:53:47 -0800

    added project files

---
+++ toy-problems-cpp/toy-problems-cpp/toy_problems_cpp.h
@@ -0,0 +1,23 @@
+/*
+ *  toy_problems_cpp.h
+ *  toy-problems-cpp
+ *
+ *  Created by David Raleigh on 1/22/15.
+ *  Copyright (c) 2015 David Raleigh. All rights reserved.
+ *
+ */
+
+#ifndef toy_problems_cpp_
+#define toy_problems_cpp_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class toy_problems_cpp
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ toy-problems-cpp/toy-problems-cpp/toy_problems_cppPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  toy_problems_cppPriv.h
+ *  toy-problems-cpp
+ *
+ *  Created by David Raleigh on 1/22/15.
+ *  Copyright (c) 2015 David Raleigh. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class toy_problems_cppPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
