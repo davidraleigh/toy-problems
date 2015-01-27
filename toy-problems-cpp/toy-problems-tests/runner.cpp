50cf72f36494081f82ed44aa9ba5a14891a967b6
David Raleigh <davidraleigh@gmail.com>
2015-01-26 16:32:59 -0800

    fixed cxxtest call to use python

---
+++ toy-problems-cpp/toy-problems-tests/runner.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  toy-problems-tests
+//
+//  Created by David Raleigh on 1/22/15.
+//  Copyright (c) 2015 David Raleigh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ toy-problems-cpp/toy-problems-tests/toy-problem-tests.h
@@ -0,0 +1,13 @@
+//
+//  toy-problem-tests.h
+//  toy-problems-cpp
+//
+//  Created by David Raleigh on 1/22/15.
+//  Copyright (c) 2015 David Raleigh. All rights reserved.
+//
+
+#ifndef toy_problems_cpp_toy_problem_tests_h
+#define toy_problems_cpp_toy_problem_tests_h
+
+
+#endif
